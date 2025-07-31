8d358f42495332b16f32d5cd7232379d0d106778
aaronBIOO <aaronnabioo@gmail.com>
2025-07-31 16:05:26 +0000

    Update: CTA and pricing sections implemented on app

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import Layout from "@/components/Layout";
 import { Hero } from "@/components/sections/Hero";
 import { Brands } from "@/components/sections/Brands";
 import { Services } from "@/components/sections/Services";
-import { AboutUs } from "./components/sections/AboutUs";
+import { AboutUs } from "@/components/sections/AboutUs";
+import { CTA } from "@/components/sections/CallToAction";
+import { Pricing } from "@/components/sections/Pricing";
 
 function App() {
 
   return (
     <Layout title="theFuture">
       <Hero />
-      <Brands />
+      <Brands />  
       <Services />
       <AboutUs />
+      <Pricing />
+      <CTA />
     </Layout>
   )
 }
